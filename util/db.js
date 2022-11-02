95ea199a93b36864d348b203986af8cf25906cb5
natty <forProjectsMe@gmail.com>
2022-11-02 12:16:09 +0300

    j

--- util/db.js
+++ util/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('', `freedb_natty7`, `jyTSmgJ3@Mh8ESS`, {
+const sequelize = new Sequelize('', `freedb_natty7`, `qHQq&f4gy26rRpb`, {
 	dialect: 'mysql',
 	host: 'sql.freedb.tech',
 	database: `freedb_wabegotte_new`,
